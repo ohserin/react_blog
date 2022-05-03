8cd1bf0cba4542927065592b98e7f85a3e6728b3
ohserin <97070004+ohserin@users.noreply.github.com>
2022-05-03 18:01:11 +0900

    22.05.03

--- api/index.js
+++ api/index.js
@@ -3,17 +3,19 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
 
 mongoose
     .connect(process.env.MONGO_URL)
-    .then(console.log("몽고DB랑 연결이 되었습니다."))
+    .then(console.log("connected to mongo"))
     .catch((err) => console.log(err));
 
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
-app.listen(5050, () => {
+app.listen(5060, () => {
     console.log("Backend is running");
 });
